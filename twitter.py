e36ab5cea0c5ab3ade9729f6b1fcef5459d5cfff
mavillard <ajimene6@uwo.ca>
2015-02-18 16:31:37 -0500

    Followers

--- twitter.py
+++ twitter.py
@@ -1,86 +1,28 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
+import tweepy
 
-import twitter
-import time
-import serial
-
-#Functions
-def getCommanders():
-    commanders = api.GetFriends()
-    commanders.append(api.GetUser(user))
-    return commanders
-
-def isCommand(p,u):
-    if p.text.count('$') == 2:
-        if u.screen_name == user:
-            result = True
-        elif '@'+ user in p.text:
-            result = True
-        else:
-            result = False
-    else:
-        result = False
-    return result
-
-def getLastCommands():
-    commands = {}
-    for c in getCommanders():
-        timeline = api.GetUserTimeline(c.screen_name)
-        if timeline:
-            new = timeline[0]
-            if isCommand(new,c):
-                commands[c] = new
-    return commands
-
-def extractInstruction(s):
-    ini = s.find('$')
-    sub = s[ini+1:]
-    end = sub.find('$')
-    sub = sub[:end]
-    return sub
-
-def getCommand():
-    command = []
-    last_commands = getLastCommands()
-    if last_commands:
-        u = last_commands.keys()[0]
-        i = last_commands[u]
-        command.append(u.screen_name)
-        command.append(extractInstruction(i.text))
-        command.append(i.id)
-    return command
-
-def executed(command):
-    return exec_commands and command[2] in exec_commands
-    
-def arduino_send(s):
-    for c in s:
-        arduino.write(c)
+import credentials
 
 
-#Authenticate yourself with twitter
-ck = t_consumer_key
-cs = t_consumer_secret
-atk = t_access_token
-ats = t_access_token_secret
-api = twitter.Api(ck,cs,atk,ats)
-#User
-user = 'H9832B'
+# AUTHENTICATION
+consumer_key = credentials.t_consumer_key
+consumer_secret = credentials.t_consumer_secret
+access_token = credentials.t_access_token
+access_token_secret = credentials.t_access_token_secret
 
-#Arduino
-arduino = serial.Serial('/dev/ttyACM0',9600,timeout=1)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-#Begin
-exec_commands = []
-while True:
-    command = getCommand()
-    if len(command) > 0 and not executed (command):
-        msg = command[0] + " -> " + command[1]
-        arduino_send("@"+msg+"%")
-        exec_commands.append(command[2])
-        print "user: " + command[0] + ", command: " + command[1]
-    else:
-        print "No new commands."
-    time.sleep(5)
-#End
+
+# API
+api = tweepy.API(auth)
+
+
+# EXAMPLES
+# Followers
+for f in api.followers('theculturist_ca'):
+    print 'Handle: @' + f.screen_name
+    print 'Full name: ' + f.name
+    print '====================================='
+
+
+
