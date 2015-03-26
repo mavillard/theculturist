c9b2d836e37fa35c20c4452ea88687b27501e01b
mavillard <ajimene6@uwo.ca>
2015-03-26 00:26:12 -0400

    Done!

--- theculturister.py
+++ theculturister.py
@@ -36,18 +36,6 @@ def to_ascii(s):
         result = s.encode('utf-8')
     return result
 
-def format_date_fb_csv(date_time):
-    date = date_time.split(' ')[0]
-    (month, day, year) = map(int, date.split('/'))
-    formatted_date = '{}-{}-{}'.format(year, month, day)
-    return formatted_date
-
-def format_date_fb_api(date_time):
-    date = date_time.split('T')[0]
-    (year, month, day) = map(int, date.split('-'))
-    formatted_date = '{}-{}-{}'.format(year, month, day)
-    return formatted_date
-
 def clean_url(url):
     if url.startswith('https://'):
         url = url[8:]
@@ -199,6 +187,18 @@ def process_google(writers):
             break
 
 # FACEBOOK
+def format_date_fb_csv(date_time):
+    date = date_time.split(' ')[0]
+    (month, day, year) = map(int, date.split('/'))
+    formatted_date = '{}-{}-{}'.format(year, month, day)
+    return formatted_date
+
+def format_date_fb_api(date_time):
+    date = date_time.split('T')[0]
+    (year, month, day) = map(int, date.split('-'))
+    formatted_date = '{}-{}-{}'.format(year, month, day)
+    return formatted_date
+
 def process_likers(writers, post_id, likes):
     for like in likes:
         user_id = like['id']
@@ -216,7 +216,7 @@ def process_commenters(writers, post_id, comments):
     for comment in comments:
         user_id = comment['from']['id']
         name = comment['from']['name']
-        date = comment['created_time']
+#        date = comment['created_time']
         # If the user does not exist create the user
         if user_id not in fb_user_ids:
             user_type = 'FacebookUser'
@@ -232,7 +232,7 @@ def process_commenters(writers, post_id, comments):
             #date
         ])
 
-def process_mentions(writers, post_id, mentions):
+def process_fbmentions(writers, post_id, mentions):
     for mention in mentions:
         user_id = mention['id']
         name = mention['name']
@@ -254,6 +254,7 @@ def process_facebook(writers):
     # Page
     TOTAL_LIKES = 440
     writers['FacebookPage'].writerow([2, 'FacebookPage', 'theculturist_ca', TOTAL_LIKES])
+    writers['fb_related_to'].writerow([2, 1, 'fb_related_to'])
     
     # Posts
     fb_api = facebook.GraphAPI(credentials.FB_ACCESS_TOKEN)
@@ -298,17 +299,17 @@ def process_facebook(writers):
                 post_id,
                 post_type,
                 comments,
+                date,
                 engagement,
                 likes,
                 permalink,
-                date,
                 reach,
                 shares,
                 text
             ])
             
             # Link the post to the facebook page
-            writers['article_belongs_to'].writerow([
+            writers['post_belongs_to'].writerow([
                 post_id,
                 2,
                 'post_belongs_to'
@@ -331,15 +332,70 @@ def process_facebook(writers):
             if 'to' in post:
                 data = post['to']['data']
                 # Process the users mentioned on this post
-                process_mentions(writers, post_id, data)
+                process_fbmentions(writers, post_id, data)
         else:
             break
 
 # TWITTER
+def format_date_tw_csv(date_time):
+    date = date_time.split(' ')[0]
+    (year, month, day) = map(int, date.split('-'))
+    formatted_date = '{}-{}-{}'.format(year, month, day)
+    return formatted_date
+
+def format_date_tw_api(date_time):
+    year = date_time.year
+    month = date_time.month
+    day = date_time.day
+    formatted_date = '{}-{}-{}'.format(year, month, day)
+    return formatted_date
+
+def process_retweeters(writers, tweet_id, retweets):
+    for rt in retweets:
+        user_id = rt.author.id
+        handle = rt.author.screen_name
+        name = rt.author.name
+#        date = rt.created_at
+        # If the user does not exist create the user
+        if user_id not in tw_user_ids:
+            user_type = 'TwitterUser'
+            handle = to_ascii(handle)
+            name = to_ascii(name)
+            writers['TwitterUser'].writerow([user_id, user_type, handle, name])
+            tw_user_ids.add(user_id)
+        # Create the relation
+#        date = format_date_tw_api(date)
+        writers['twuser_retweets'].writerow([
+            user_id,
+            tweet_id,
+            'twuser_retweets'
+#            date
+        ])
+
+def process_twmentions(writers, tweet_id, mentions):
+    for mention in mentions:
+        user_id = mention['id']
+        handle = mention['screen_name']
+        name = mention['name']
+        # If the user does not exist create the user
+        if user_id not in tw_user_ids:
+            user_type = 'TwitterUser'
+            handle = to_ascii(handle)
+            name = to_ascii(name)
+            writers['TwitterUser'].writerow([user_id, user_type, handle, name])
+            tw_user_ids.add(user_id)
+        # Create the relation
+        writers['tweet_mentions'].writerow([
+            tweet_id,
+            user_id,
+            'tweet_mentions'
+        ])
+
 def process_twitter(writers):
     # Account
     TOTAL_FOLLOWERS = 415
     writers['TwitterAccount'].writerow([3, 'TwitterAccount', 'theculturist_ca', TOTAL_FOLLOWERS])
+    writers['tw_related_to'].writerow([3, 1, 'tw_related_to'])
     
     # Tweets
     consumer_key = credentials.TW_CONSUMER_KEY
@@ -357,72 +413,67 @@ def process_twitter(writers):
             tweet_id = row[0]
             permalink = row[1]
             text = row[2]
-            date = row[6]
-            reach = row[7]
-            engagement = row[13]
+            date = row[3]
+            impression = row[4]
+            engagement = row[5]
+            retweets = row[7]
+            replies = row[8]
+            favorites = row[9]
             
-            post_type = 'Post'
+            tweet = tw_api.get_status(tweet_id)
+            
+            # Hashtags
+            if 'hashtags' in tweet.entities:
+                hashtags = tweet.entities['hashtags']
+                hashtags = [ht['text'] for ht in hashtags]
+            
+            tweet_type = 'Tweet'
             text = to_ascii(text)
-            date = format_date_fb_csv(date)
-            
-            post = fb_api.get_object(post_id)
-            if 'likes' in post:
-                data = post['likes']['data']
-                likes = len(data)
-                # Process the users that liked this post
-                process_likers(writers, post_id, data)
-            else:
-                likes = 0
-            if 'comments' in post:
-                data = post['comments']['data']
-                comments = len(data)
-                # Process the users that commented on this post
-                process_commenters(writers, post_id, data)
-            else:
-                comments = 0
-            if 'shares' in post:
-                shares = post['shares']['count']
-            else:
-                shares = 0
-            
-            writers['Post'].writerow([
-                post_id,
-                post_type,
-                comments,
-                engagement,
-                likes,
-                permalink,
+            date = format_date_tw_csv(date)
+            writers['Tweet'].writerow([
+                tweet_id,
+                tweet_type,
                 date,
-                reach,
-                shares,
+                engagement,
+                favorites,
+                hashtags,
+                impression,
+                permalink,
+                replies,
+                retweets,
                 text
             ])
             
-            # Link the post to the facebook page
-            writers['article_belongs_to'].writerow([
-                post_id,
-                2,
-                'post_belongs_to'
+            # Link the tweet to the twitter account
+            writers['tweet_belongs_to'].writerow([
+                tweet_id,
+                3,
+                'tweet_belongs_to'
             ])
             
             # Link the post to the article
-            urls = extract_urls(text)
-            for url in urls:
-                response = urllib.urlopen(url)
-                if response.getcode() == 200:
-                    url = clean_url(response.url)
-                    if url in article_ids:
-                        writers['post_links_to'].writerow([
-                            post_id,
-                            url,
-                            'post_links_to'
-                        ])
+            if 'urls' in tweet.entities:
+                urls = tweet.entities['urls']
+                for url_dict in urls:
+                    url = url_dict['url']
+                    response = urllib.urlopen(url)
+                    if response.getcode() == 200:
+                        url = clean_url(response.url)
+                        if url in article_ids:
+                            writers['tweet_links_to'].writerow([
+                                tweet_id,
+                                url,
+                                'tweet_links_to'
+                            ])
             
-            # Link the post to the mentioned users
-            if 'to' in post:
-                data = post['to']['data']
-                # Process the users mentioned on this post
-                process_mentions(writers, post_id, data)
+            # Retweets
+            retweets = tweet.retweets()
+            process_retweeters(writers, tweet_id, retweets)
+            
+            # Mentions
+            if 'user_mentions' in tweet.entities:
+                mentions = tweet.entities['user_mentions']
+                process_twmentions(writers, tweet_id, mentions)
         else:
             break
 
@@ -430,8 +481,11 @@ def process_twitter(writers):
 # MAIN
 def main():
     writers = prepare_sylva()
+    print 'Processing Google Analytics...'
     process_google(writers)
+    print 'Processing Facebook Insights...'
     process_facebook(writers)
+    print 'Processing Twitter Analytics...'
     process_twitter(writers)
 
 main()
